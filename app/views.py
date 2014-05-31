64fd5c8876c787285a5543ca70f8a1c00e558b41
Ilmari Aalto <ilmari.aalto@iki.fi>
2014-05-31 23:42:42 +0100

    adding forms etc

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = { 'nickname': 'Ilmari' }
     posts = [ # fake array of posts
@@ -20,4 +20,11 @@ def index():
     title = 'Home',
     user = user,
     posts = posts)
+    
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form)
 
